3e534a66c1d645c07e68c73de5f8b9302a3dbbd3
Fedor <lfa1@yandex.ru>
2022-07-19 00:06:49 +0300

    Update pulse.c

--- src/pulse.c
+++ src/pulse.c
@@ -1,14 +1,12 @@
 #include "tl_common.h"
-#include "drivers/8258/gpio_8258.h"
-#include "application/print/u_printf.h"
+//#include "drivers/8258/gpio_8258.h"
+//#include "application/print/u_printf.h"
 
 #include "app_adc.h"
 #include "pulse.h"
 #include "cfg.h"
 
-//#define BIT_COUNT   32768
-#define BIT_COUNT   1024
-#define TASK_COUNT  (BIT_COUNT/2+BIT_COUNT)
+#define BIT_COUNT   256
 
 _attribute_data_retention_ static water_counter_t hot_counter;
 _attribute_data_retention_ static water_counter_t cold_counter;
@@ -28,8 +26,6 @@ _attribute_ram_code_ void pulse_init() {
 
 _attribute_ram_code_ static void water_counters() {
 
-    gpio_setup_up_down_resistor(HOT_GPIO, PM_PIN_PULLUP_1M);
-
     if (!gpio_read(HOT_GPIO)) {
         if (hot_counter.bit == BIT_COUNT) {
             hot_counter.state = true;
@@ -44,8 +40,6 @@ _attribute_ram_code_ static void water_counters() {
         }
     }
 
-    gpio_setup_up_down_resistor(HOT_GPIO, PM_PIN_UP_DOWN_FLOAT);
-
     if (hot_counter.state == true) {
 
         if (hot_counter.pressed == false) {
@@ -55,8 +49,6 @@ _attribute_ram_code_ static void water_counters() {
     } else {
         hot_counter.pressed = false;
     }
-
-    gpio_setup_up_down_resistor(COLD_GPIO, PM_PIN_PULLUP_1M);
 
     if (!gpio_read(COLD_GPIO)) {
         if (cold_counter.bit == BIT_COUNT) {
@@ -72,8 +64,6 @@ _attribute_ram_code_ static void water_counters() {
         }
     }
 
-    gpio_setup_up_down_resistor(COLD_GPIO, PM_PIN_UP_DOWN_FLOAT);
-
     if (cold_counter.state == true) {
 
         if (cold_counter.pressed == false) {
@@ -85,13 +75,10 @@ _attribute_ram_code_ static void water_counters() {
     }
 }
 
-_attribute_ram_code_ bool task_counters() {
-    bool save_config = false;
+_attribute_ram_code_ uint8_t task_counters() {
+    uint8_t save_config = false;
 
-//    do {
-        water_counters();
-//        printf("hot_counter.bit - 0x%X\r\n", hot_counter.bit);
-//    } while (hot_counter.pressed == true || cold_counter.pressed == true);
+    water_counters();
 
     if (hot_counter.count) {
         save_config = true;
@@ -109,6 +96,10 @@ _attribute_ram_code_ bool task_counters() {
 #if UART_PRINT_DEBUG_ENABLE
         printf("cold counter - %u\r\n", get_coldwater());
 #endif /* UART_PRINT_DEBUG_ENABLE */
+    }
+
+    if (save_config) {
+        write_config();
     }
 
     return save_config;
