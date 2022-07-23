920c9e36fbf1649d9e353fb006911be1b6428c91
Fedor <lfa1@yandex.ru>
2022-07-24 00:40:24 +0300

    Update cfg.h

--- src/include/cfg.h
+++ src/include/cfg.h
@@ -14,18 +14,18 @@ typedef struct __attribute__((packed)) _counters_config_t {
 } counters_config_t;
 
 typedef struct __attribute__((packed)) _watermeter_config_t {
-    uint32_t id;                /* 1st place only! ID - ID_CONFIG       */
-    uint8_t  active;            /* 2st place only! current active cfg   */
-    uint32_t flash_addr;        /* 3st place only! flash page address   */
-    counters_config_t counters; /* 4st place only! water counters       */
-    uint8_t  size;              /* 5st place only! sizeof config        */
-    uint8_t  liters_per_pulse;  /* liters per pulse                     */
-    uint8_t  ble_name[32];      /* Module name with MAC                 */
-    uint8_t  wl_mac1[6];        /* MAC1 for whitelist                   */
-    uint8_t  wl_mac2[6];        /* MAC2 for whitelist                   */
-    uint8_t  wl_mac3[6];        /* MAC3 for whitelist                   */
-    uint8_t  wl_mac4[6];        /* MAC4 for whitelist                   */
-    uint8_t  whitelist_enable;  /* 0 - disable, 1-4 enable              */
+    uint32_t id;                        /* 1st place only! ID - ID_CONFIG       */
+    uint8_t  active;                    /* 2st place only! current active cfg   */
+    uint32_t flash_addr;                /* 3st place only! flash page address   */
+    counters_config_t counters;         /* 4st place only! water counters       */
+    uint8_t  size;                      /* 5st place only! sizeof config        */
+    uint8_t  liters_per_pulse;          /* liters per pulse                     */
+    uint8_t  wl_mac1[6];                /* MAC1 for whitelist                   */
+    uint8_t  wl_mac2[6];                /* MAC2 for whitelist                   */
+    uint8_t  wl_mac3[6];                /* MAC3 for whitelist                   */
+    uint8_t  wl_mac4[6];                /* MAC4 for whitelist                   */
+    uint8_t  whitelist_enable;          /* 0 - disable, 1-4 enable              */
+    uint8_t  ble_name[DEV_NAME_SIZE];   /* Module name with MAC                 */
 //    uint8_t  test;
 } watermeter_config_t;
 
