085c329ff97f5bea6a1e9d3341383ced194fa704
Fedor <lfa1@yandex.ru>
2022-07-25 16:56:59 +0300

    Update ble.h

--- src/include/ble.h
+++ src/include/ble.h
@@ -68,15 +68,16 @@ typedef struct __attribute__((packed)) _adv_data_t {
 } adv_data_t;
 
 extern uint8_t ble_connected;
+extern uint8_t ota_is_working;
 extern uint8_t mac_public[6], mac_random_static[6];
 extern uint8_t ble_name[DEV_NAME_SIZE];
 extern adv_data_t adv_data;
 
 void init_ble();
 void set_adv_data();
-void ble_send_battery();
-void blt_pm_proc(void);
 void ev_adv_timeout(u8 e, u8 *p, int n);
-
+void ble_send_battery();
+void ble_send_hotwater();
+void ble_send_coldwater();
 
 #endif /* SRC_INCLUDE_BLE_H_ */
