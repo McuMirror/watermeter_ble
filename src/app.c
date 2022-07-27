bd42e44c5331f0d86ce2560c3f2b74a9f3549176
Fedor <lfa1@yandex.ru>
2022-07-27 23:55:43 +0300

    Update app.c

--- src/app.c
+++ src/app.c
@@ -12,7 +12,7 @@
 #include "ble.h"
 
 #if UART_PRINT_DEBUG_ENABLE
-//_attribute_data_retention_ static uint32_t deepRetn_count = 0;
+//_attribute_data_retention_ static uint32t deepRetn_count = 0;
 #endif /* UART_PRINT_DEBUG_ENABLE */
 
 #define UPDATE_PERIOD       5000UL      /* 5 sec */
@@ -20,9 +20,7 @@
 _attribute_data_retention_  uint32_t update_interval  = 0;
 _attribute_data_retention_  uint32_t battery_interval = 0;
 
-
-void user_init_normal(void)
-{
+void user_init_normal(void) {
 
 #if UART_PRINT_DEBUG_ENABLE
     printf("Start user_init_normal()\r\n");
@@ -32,16 +30,14 @@ void user_init_normal(void)
     check_battery();
 	random_generator_init();  //this is must
     pulse_init();
-
     init_config();
     init_ble();
-
 }
 
 _attribute_ram_code_ void user_init_deepRetn(void) {
 
 #if UART_PRINT_DEBUG_ENABLE
-//    printf("%u Start user_init_deepRetn()\r\n", ++deepRetn_count);
+//    printf("%u Start user_init_deeptn()\r\n", ++deepRetn_count);
 #endif /* UART_PRINT_DEBUG_ENABLE */
 
 	blc_ll_initBasicMCU();   //mandatory
@@ -63,11 +59,6 @@ _attribute_ram_code_ void blt_pm_proc(void)
 }
 
 
-
-_attribute_data_retention_ uint8_t send_battery = 0;
-_attribute_data_retention_ uint8_t send_hot     = 0;
-_attribute_data_retention_ uint8_t send_cold    = 0;
-
 void main_loop (void) {
 
     if (task_counters()) {
@@ -82,6 +73,9 @@ void main_loop (void) {
             check_battery();
 
             if (battery_level != adv_data.adv_battery.level) {
+#if UART_PRINT_DEBUG_ENABLE
+        printf("b_level - %u, adv_b_level - %u\r\n", battery_level, adv_data.adv_battery.level);
+#endif /* UART_PRINT_DEBUG_ENABLE */
                 adv_data.adv_battery.level = battery_level;
                 set_adv_data();
             }
@@ -104,14 +98,6 @@ void main_loop (void) {
         }
 
         update_interval = clock_time();
-    }
-
-    if (battery_level != adv_data.adv_battery.level) {
-#if UART_PRINT_DEBUG_ENABLE
-        printf("b_level - %u, adv_ b_level - %u\r\n", battery_level, adv_data.adv_battery.level);
-#endif /* UART_PRINT_DEBUG_ENABLE */
-        adv_data.adv_battery.level = battery_level;
-        set_adv_data();
     }
 
     blt_sdk_main_loop();
