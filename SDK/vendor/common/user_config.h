420bf7de22141032afa5c3c4cf4686d8be9580bb
Fedor <lfa1@yandex.ru>
2022-08-29 07:47:23 +0300

    update SDK

--- SDK/vendor/common/user_config.h
+++ SDK/vendor/common/user_config.h
@@ -45,4 +45,31 @@
  *******************************************************************************************************/
 #pragma once
 
+//#if (__PROJECT_8255_BLE_REMOTE__)
+//	#include "../b85m_ble_remote/app_config.h"
+//#elif (__PROJECT_8258_BLE_REMOTE__ || __PROJECT_8278_BLE_REMOTE__)
+//	#include "../b85m_ble_remote/app_config.h"
+//#elif (__PROJECT_8258_BLE_SAMPLE__ || __PROJECT_8278_BLE_SAMPLE__)
+//	#include "../b85m_ble_sample/app_config.h"
+//#elif (__PROJECT_8258_MODULE__ || __PROJECT_8278_MODULE__)
+//	#include "../b85m_module/app_config.h"
+//#elif (__PROJECT_8258_HCI__ || __PROJECT_8278_HCI__)
+//	#include "../b85m_hci/app_config.h"
+//#elif (__PROJECT_8258_FEATURE_TEST__ || __PROJECT_8278_FEATURE_TEST__)
+//	#include "../b85m_feature_test/app_config.h"
+//#elif(__PROJECT_8258_MASTER_KMA_DONGLE__ || __PROJECT_8278_MASTER_KMA_DONGLE__ )
+//	#include "../b85m_master_kma_dongle/app_config.h"
+//#elif(__PROJECT_8258_MASTER_SLAVE__ )
+//	#include "../b85m_master_slave/app_config.h"
+//#elif(__PROJECT_8258_BQB_LOWER_TESTER__ )
+//	#include "../b85m_bqb_lowertester/app_config.h"
+//#elif(__PROJECT_8258_INTERNAL_TEST__ ||  __PROJECT_8278_INTERNAL_TEST__)
+//	#include "../b85m_internal_test/app_config.h"
+//#elif(__PROJECT_8258_DRIVER_TEST__ ||  __PROJECT_8278_DRIVER_TEST__)
+//	#include "../b85m_driver_test/app_config.h"
+//#else
+//	#include "../common/default_config.h"
+//#endif
+
 #include "app_config.h"
+
