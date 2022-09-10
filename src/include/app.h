12388d83ce0254d5b10ba5f141010088e765b906
Fedor <lfa1@yandex.ru>
2022-09-11 00:24:53 +0300

    Update app.h

--- src/include/app.h
+++ src/include/app.h
@@ -5,6 +5,10 @@
 
 extern uint32_t conn_timeout;
 extern uint32_t time_sec;
+extern uint8_t battery_change;
+extern uint8_t hot_change;
+extern uint8_t cold_change;
+
 
 void user_init_normal(void);
 void user_init_deepRetn(void);
