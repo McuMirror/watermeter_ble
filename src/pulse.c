74e2637653ebac6f8cefbd86d023ce14c51c499b
Fedor <lfa1@yandex.ru>
2022-09-13 19:03:03 +0300

    Update pulse.c

--- src/pulse.c
+++ src/pulse.c
@@ -93,38 +93,45 @@ _attribute_ram_code_ static void water_counters() {
 _attribute_ram_code_ uint8_t task_counters() {
     uint8_t save_config = false;
 
-    for (uint16_t i = 0; i < TASK_COUNT; i++) {
-        water_counters();
-        sleep_ms(3);
+    if ((!gpio_read(HOT_GPIO) && hot_counter.bit == 1) ||
+         (gpio_read(HOT_GPIO) && hot_counter.bit == BIT_COUNT) ||
+         (!gpio_read(COLD_GPIO) && cold_counter.bit == 1) ||
+         (gpio_read(COLD_GPIO) && cold_counter.bit ==BIT_COUNT)) {
+
+        for (uint16_t i = 0; i < TASK_COUNT; i++) {
+            water_counters();
+            sleep_ms(3);
+        }
+
+        if (hot_counter.count) {
+            save_config = true;
+            /* detect hot counter overflow */
+            watermeter_config.counters.hot_water_count = check_counter_overflow(watermeter_config.counters.hot_water_count +
+                    (hot_counter.count * watermeter_config.liters_per_pulse));
+            hot_counter.count = 0;
+            hot_notify = NOTIFY_MAX;
+    #if UART_PRINT_DEBUG_ENABLE
+            printf("hot counter - %u\r\n", watermeter_config.counters.hot_water_count);
+    #endif /* UART_PRINT_DEBUG_ENABLE */
+        }
+
+        if (cold_counter.count) {
+            save_config = true;
+            /* detect cold counter overflow */
+            watermeter_config.counters.cold_water_count = check_counter_overflow(watermeter_config.counters.cold_water_count +
+                    (cold_counter.count * watermeter_config.liters_per_pulse));
+            cold_counter.count = 0;
+            cold_notify = NOTIFY_MAX;
+            #if UART_PRINT_DEBUG_ENABLE
+            printf("cold counter - %u\r\n", watermeter_config.counters.cold_water_count);
+    #endif /* UART_PRINT_DEBUG_ENABLE */
+        }
+
+        if (save_config) {
+            write_config();
+        }
     }
 
-    if (hot_counter.count) {
-        save_config = true;
-        /* detect hot counter overflow */
-        watermeter_config.counters.hot_water_count = check_counter_overflow(watermeter_config.counters.hot_water_count +
-                (hot_counter.count * watermeter_config.liters_per_pulse));
-        hot_counter.count = 0;
-        hot_notify = NOTIFY_MAX;
-#if UART_PRINT_DEBUG_ENABLE
-        printf("hot counter - %u\r\n", watermeter_config.counters.hot_water_count);
-#endif /* UART_PRINT_DEBUG_ENABLE */
-    }
-
-    if (cold_counter.count) {
-        save_config = true;
-        /* detect cold counter overflow */
-        watermeter_config.counters.cold_water_count = check_counter_overflow(watermeter_config.counters.cold_water_count +
-                (cold_counter.count * watermeter_config.liters_per_pulse));
-        cold_counter.count = 0;
-        cold_notify = NOTIFY_MAX;
-        #if UART_PRINT_DEBUG_ENABLE
-        printf("cold counter - %u\r\n", watermeter_config.counters.cold_water_count);
-#endif /* UART_PRINT_DEBUG_ENABLE */
-    }
-
-    if (save_config) {
-        write_config();
-    }
 
     return save_config;
 }
