091d0302aa2f2d6107b75269f3c8e2e52324e567
Fedor <lfa1@yandex.ru>
2022-09-14 11:52:07 +0300

    Update app_config.h

--- src/include/app_config.h
+++ src/include/app_config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define VERSION                     "V1.5"  /* decimal only                 */
+#define VERSION                     0x16    /* BCD format (0x10 -> '1.0')   */
 #define WATERMETER_ID               0xFED5  /* uint16_t                     */
 #define DEV_NAME_CHR                'W','a','t','e','r','m','e','t','e','r','_','B','L','E'
 #define DEV_NAME_STR                "Watermeter_BLE"
