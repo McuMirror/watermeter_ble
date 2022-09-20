6aacf5b06e43140dfe2297740b959ad4be039239
Fedor <lfa1@yandex.ru>
2022-09-20 11:19:18 +0300

    Update log.h

--- src/include/log.h
+++ src/include/log.h
@@ -2,16 +2,16 @@
 #define SRC_INCLUDE_LOG_H_
 
 #define LOG_ID        0x78
-#define LOG_STR_SIZE  13
-#define LOG_BUFF_SIZE 1024
-#define LOG_BUFF_MASK LOG_BUFF_SIZE-1
+#define LOG_STR_SIZE  13              /* all 16 bytes. 1 byte id, 1 byte pid, 1 byte debug_enable. 16-3=13 bytes */
+#define LOG_BUFF_SIZE 1024            /* size ring buffer  */
+#define LOG_BUFF_MASK LOG_BUFF_SIZE-1 /* mask ring buffer  */
 
 /* not more 16 bytes!!! */
 typedef struct __attribute__((packed)) _log_notify_t {
-    uint8_t  id;                        /* LOG_ID            */
-    uint8_t  pid;                       /* 0 .. 255          */
-    uint8_t  debug_enabled;             /* 1 - debug enabled */
-    uint8_t  str[LOG_STR_SIZE];         /* log string        */
+    uint8_t  id;                      /* LOG_ID            */
+    uint8_t  pid;                     /* 0 .. 255          */
+    uint8_t  debug_enabled;           /* 1 - debug enabled */
+    uint8_t  str[LOG_STR_SIZE];       /* log string        */
 } log_notify_t;
 
 extern uint8_t log_pos;
