e83899050dff5bb3c995a8fcc443d6d2df9ab4c1
Fedor <lfa1@yandex.ru>
2023-02-17 07:44:34 +0300

    Update pulse.c

--- src/pulse.c
+++ src/pulse.c
@@ -10,8 +10,13 @@
 #define BIT_COUNT   64                          /* number of polls for debounce */
 #define TASK_COUNT  (BIT_COUNT/2+BIT_COUNT)     /* task duration                */
 
-_attribute_data_retention_ static water_counter_t hot_counter;
-_attribute_data_retention_ static water_counter_t cold_counter;
+#define ST_CLOSE    1
+#define ST_OPEN     0
+#define ST_PRESSED  1
+#define ST_RELEASE  0
+
+_attribute_data_retention_ static status_water_counter_t status_hot_counter;
+_attribute_data_retention_ static status_water_counter_t status_cold_counter;
 _attribute_data_retention_ uint8_t save_config = false;
 
 _attribute_ram_code_ uint32_t check_counter_overflow(uint32_t check_count) {
@@ -31,22 +36,22 @@ _attribute_ram_code_ uint32_t check_counter_overflow(uint32_t check_count) {
 
 _attribute_ram_code_ void init_pulse() {
 
-    hot_counter.status = 0;
+    status_hot_counter.status = ST_OPEN;
     if (!gpio_read(HOT_GPIO)) {
-        hot_counter.bit = BIT_COUNT;
-        hot_counter.pressed = 1;
+        status_hot_counter.bit = BIT_COUNT;
+        status_hot_counter.pressed = ST_PRESSED;
     } else {
-        hot_counter.bit = 1;
-        hot_counter.pressed = 0;
+        status_hot_counter.bit = 1;
+        status_hot_counter.pressed = ST_RELEASE;
     }
 
-    cold_counter.status = 0;
+    status_cold_counter.status = ST_OPEN;
     if (!gpio_read(COLD_GPIO)) {
-        cold_counter.bit = BIT_COUNT;
-        cold_counter.pressed = 1;
+        status_cold_counter.bit = BIT_COUNT;
+        status_cold_counter.pressed = ST_PRESSED;
     } else {
-        cold_counter.bit = 1;
-        cold_counter.pressed = 0;
+        status_cold_counter.bit = 1;
+        status_cold_counter.pressed = ST_RELEASE;
     }
 
 }
@@ -54,22 +59,22 @@ _attribute_ram_code_ void init_pulse() {
 _attribute_ram_code_ static void water_counters() {
 
     if (!gpio_read(HOT_GPIO)) {
-        if (hot_counter.bit == BIT_COUNT) {
-            hot_counter.status = 1;
+        if (status_hot_counter.bit == BIT_COUNT) {
+            status_hot_counter.status = ST_CLOSE;
         } else {
-            hot_counter.bit <<= 1;
+            status_hot_counter.bit <<= 1;
         }
     } else {
-        if (hot_counter.bit == 1) {
-            hot_counter.status = 0;
+        if (status_hot_counter.bit == 1) {
+            status_hot_counter.status = ST_OPEN;
         } else {
-            hot_counter.bit >>= 1;
+            status_hot_counter.bit >>= 1;
         }
     }
-    if (hot_counter.status) {
+    if (status_hot_counter.status) {
 
-        if (hot_counter.pressed == 0) {
-            hot_counter.pressed = 1;
+        if (!status_hot_counter.pressed) {
+            status_hot_counter.pressed = ST_PRESSED;
             save_config = true;
             /* detect hot counter overflow */
             watermeter_config.counters.hot_water_count =
@@ -81,26 +86,26 @@ _attribute_ram_code_ static void water_counters() {
 #endif /* UART_PRINT_DEBUG_ENABLE */
         }
     } else {
-        hot_counter.pressed = 0;
+        status_hot_counter.pressed = ST_RELEASE;
     }
 
     if (!gpio_read(COLD_GPIO)) {
-        if (cold_counter.bit == BIT_COUNT) {
-            cold_counter.status = 1;
+        if (status_cold_counter.bit == BIT_COUNT) {
+            status_cold_counter.status = ST_CLOSE;
         } else {
-            cold_counter.bit <<= 1;
+            status_cold_counter.bit <<= 1;
         }
     } else {
-        if (cold_counter.bit == 1) {
-            cold_counter.status = 0;
+        if (status_cold_counter.bit == 1) {
+            status_cold_counter.status = ST_OPEN;
         } else {
-            cold_counter.bit >>= 1;
+            status_cold_counter.bit >>= 1;
         }
     }
-    if (cold_counter.status) {
+    if (status_cold_counter.status) {
 
-        if (cold_counter.pressed == 0) {
-            cold_counter.pressed = 1;
+        if (!status_cold_counter.pressed) {
+            status_cold_counter.pressed = ST_PRESSED;
             save_config = true;
             /* detect cold counter overflow */
             watermeter_config.counters.cold_water_count =
@@ -112,7 +117,7 @@ _attribute_ram_code_ static void water_counters() {
 #endif /* UART_PRINT_DEBUG_ENABLE */
         }
     } else {
-        cold_counter.pressed = 0;
+        status_cold_counter.pressed = ST_RELEASE;
     }
 
 
@@ -127,10 +132,10 @@ _attribute_ram_code_ uint8_t task_counters() {
     gpio_setup_up_down_resistor(COLD_GPIO, PM_PIN_PULLUP_10K);
 #endif
 
-    if ((!gpio_read(HOT_GPIO) && hot_counter.bit == 1) ||
-        (gpio_read(HOT_GPIO) && hot_counter.bit == BIT_COUNT) ||
-        (!gpio_read(COLD_GPIO) && cold_counter.bit == 1) ||
-        (gpio_read(COLD_GPIO) && cold_counter.bit == BIT_COUNT)) {
+    if ((!gpio_read(HOT_GPIO) && status_hot_counter.bit == 1) ||
+        (gpio_read(HOT_GPIO) && status_hot_counter.bit == BIT_COUNT) ||
+        (!gpio_read(COLD_GPIO) && status_cold_counter.bit == 1) ||
+        (gpio_read(COLD_GPIO) && status_cold_counter.bit == BIT_COUNT)) {
 
         for (uint16_t i = 0; i < TASK_COUNT; i++) {
             water_counters();
