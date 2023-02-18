904c8920e9be445557d694bf9826e4ba3bf9bcf5
Fedor <lfa1@yandex.ru>
2023-02-19 00:25:11 +0300

    Update pulse.h

--- src/include/pulse.h
+++ src/include/pulse.h
@@ -1,15 +1,15 @@
 #ifndef SRC_INCLUDE_PULSE_H_
 #define SRC_INCLUDE_PULSE_H_
 
-typedef struct _status_water_conter_t {
+typedef struct _water_conter_t {
     uint8_t  status     :1;
     uint8_t  pressed    :1;
-    uint8_t  reserve    :6;
+    uint8_t  counter    :6;
     uint16_t bit;
-} status_water_counter_t;
+} water_counter_t;
 
 void init_pulse();
-bool task_counters();
+uint8_t task_counters();
 uint32_t check_counter_overflow(uint32_t check_count);
 
 #endif /* SRC_INCLUDE_PULSE_H_ */
